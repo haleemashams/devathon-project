31f578a0b1bc431652e9e1e6c6ff37399eb938f0
Haleema shams <121710915+haleemashams@users.noreply.github.com>
2023-11-02 15:42:23 +0500

    Add files via upload

---
+++ src/redux/reducers/reducer.js
@@ -0,0 +1,60 @@
+const INIT_STATE = {
+    carts: []
+};
+
+export const cartreducer = (state = INIT_STATE, action) => {
+    switch (action.type) {
+        case "ADD_CART":
+            const IteamIndex = state.carts.findIndex((item) => item.id === action.payload.id);
+
+            if (IteamIndex >= 0) {
+                state.carts[IteamIndex].qnty += 1;
+                return {
+                    ...state,
+                    carts: [...state.carts]
+                };
+            } else {
+                const temp = { ...action.payload, qnty: 1 };
+                return {
+                    ...state,
+                    carts: [...state.carts, temp]
+                };
+            }
+            // Add a 'break' statement here
+            break;
+
+        case "RMV_CART":
+            const data = state.carts.filter((el) => el.id !== action.payload);
+            return {
+                ...state,
+                carts: data
+            };
+            // Add a 'break' statement here
+            break;
+
+        case "RMV_ONE":
+            const IteamIndex_dec = state.carts.findIndex((item) => item.id === action.payload.id);
+
+            if (state.carts[IteamIndex_dec].qnty >= 1) {
+                state.carts[IteamIndex_dec].qnty -= 1;
+                console.log([...state.carts, state.carts[IteamIndex_dec].qnty]);
+
+                return {
+                    ...state,
+                    carts: [...state.carts]
+                };
+            } else if (state.carts[IteamIndex_dec].qnty === 1) {
+                const data = state.carts.filter((el) => el.id !== action.payload);
+
+                return {
+                    ...state,
+                    carts: data
+                };
+            }
+            // Add a 'break' statement here
+            break;
+
+        default:
+            return state;
+    }
+};
